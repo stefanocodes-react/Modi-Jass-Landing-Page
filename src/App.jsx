962c6b8bb16ed6d993bf0b475bde243f5f274fc6
stefano <silste45@gmail.com>
2024-08-21 18:33:40 +0300

    updated gradient

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import Header from "./components/Header.jsx";
 import Navbar from "./components/Navbar.jsx";
 function App() {
   return (
-    <div className="bg-gradient-to-b from-color-dark from-35% to-color-primary">
+    <div className="bg-gradient-to-b from-color-dark from-5% via-color-primary via-60% to-color-primary">
       <Navbar />
+      <Header />
+      <Header />
+      <Header />
       <Header />
     </div>
   );
