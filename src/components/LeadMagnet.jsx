345654a53ffa7a691617434046f4a4cedec8359c
stefano <silste45@gmail.com>
2024-08-23 14:27:44 +0300

    leadmagnet updates

--- src/components/LeadMagnet.jsx
+++ src/components/LeadMagnet.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
 import cross from "../assets/vectors/cross.svg";
 import logo from "../assets/vectors/modipfp2.png";
+const MY_FORM_ID = 6724635;
 
-const MY_FORM_ID = process.env.REACT_FORM_ID;
-// console.log(MY_FORM_ID);
 function LeadMagnet({ isOpen, toggleModal }) {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
