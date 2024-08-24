493c3de290d30d2ee6140b0c33612d0a4cc400a6
stefano <silste45@gmail.com>
2024-08-24 20:03:37 +0300

    changed nav link

--- src/components/Solution.jsx
+++ src/components/Solution.jsx
@@ -4,7 +4,7 @@ import Section from "../components/Section.jsx";
 const Solution = () => {
   return (
     <Section
-      id={"benifits"}
+      id="benifits"
       className={"gap-4 pt-0"}
       tagline={"the solution"}
       sectionTitle={"there is a better way of doing it"}
