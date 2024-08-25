f4d2e7482fa0234c141b67aafcf46d399f685731
stefano <silste45@gmail.com>
2024-08-25 12:42:46 +0300

    frequently asked questions section completed

--- src/components/FrequentlyAskedQuestions.jsx
+++ src/components/FrequentlyAskedQuestions.jsx
@@ -1,14 +1,24 @@
 import Faq from "./Faq";
 import Section from "./Section";
 import { questions } from "../constants";
-
+import graphic from "../assets/vectors/arrow-right.svg";
 const FrequentlyAskedQuestions = () => {
   return (
     <div className="flex flex-col items-center gap-8 py-16 justify-center container lg:flex-row lg:items-start lg:justify-between">
-      <Section
-        sectionTitle={`Frequently Asked Questions`}
-        className={` self-start px-0 md:max-w-[25rem] text-left py-0`}
-      />
+      <div className="flex relative flex-col items-center gap-6 justify-between">
+        <Section
+          sectionTitle={`Frequently Asked Questions`}
+          className={`py-0 self-start px-0 md:max-w-[25rem] text-left`}
+        />
+        <img
+          className="hidden lg:block lg:absolute lg:bottom-[-3.5rem] lg:right-[-1rem]"
+          src={graphic}
+          width={128}
+          height={128}
+          alt=""
+        />
+      </div>
+
       <div>
         <div className="flex flex-col justify-center items-center max-w-[40rem] gap-4">
           {questions.map((q) => (
