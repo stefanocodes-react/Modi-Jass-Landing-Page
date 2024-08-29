9a58f8666fbac61219dc56f831327edd985887d0
stefano <silste45@gmail.com>
2024-08-29 21:41:01 +0300

    removed section description from the process section

--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -5,9 +5,6 @@ function Process() {
     <Section
       sectionTitle={"My 3 Step System for Growth"}
       tagline="Growth Formula"
-      sectionDescription={[
-        `Elevate your brand in three steps Position yourself clearly, create engaging content, and craft irresistible offers.`,
-      ]}
       id={"howitworks"}
     >
       <Timeline />
