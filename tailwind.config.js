732a20791e27a39ea802e368ca72e91359266907
modi <modijass86@gmail.com>
2024-09-05 15:14:39 +0300

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ import plugin from "tailwindcss/plugin";
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/assets/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
