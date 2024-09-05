586a7494f9f348fe9088ee8af00352d4fc5c93eb
modi <modijass86@gmail.com>
2024-09-05 12:10:45 +0300

    meta tag

--- src/components/SeoImage.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import featuredImage from "../assets/vectors/modipfp3.png";
-export const FeaturedImage = () => (
-  <>
-    <meta name="og:image" content={"https://www.moayadj.com" + featuredImage} />
-  </>
-);
-
-export default FeaturedImage;
