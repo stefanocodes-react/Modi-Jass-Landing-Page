d683183096646e590a3287ddd6bdb86985c50818
modi <modijass86@gmail.com>
2024-09-05 16:47:13 +0300

    updated to property

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,17 +9,14 @@ export default function RootLayout({ children }) {
           content="Monetize on X I help you build and scale your brand create better content and sign more clients with my 3-step system in 30 days"
         />
         <meta
-          name="og:description"
+          property="og:description"
           content="Monetize on X I help you build and scale your brand create better content and sign more clients with my 3-step system in 30 days"
         />
         <meta property="og:title" content="Modi Jass | Monetize on X" />
         <meta property="og:url" content="https://www.moayadj.com/" />
         <meta property="og:site_name" content="Modi Jass" />
         <meta property="og:image" content="https://www.moayadj.com/modi.png" />
-        <meta
-          property="twitter:image"
-          content="https://www.moayadj.com/modi.png"
-        />
+        <meta name="twitter:image" content="https://www.moayadj.com/modi.png" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Modi Jass" />
         <meta
