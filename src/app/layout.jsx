53c04615e54297b78ce5485e3b4b7f0d88e10c85
modi <modijass86@gmail.com>
2024-09-05 21:59:05 +0300

    turned function to be async

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,7 +19,7 @@ export const metadata = {
     ],
   },
 };
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
