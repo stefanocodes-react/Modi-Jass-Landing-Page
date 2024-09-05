20e17858925f922c5099aa65690e960436431f86
modi <modijass86@gmail.com>
2024-09-05 15:12:14 +0300

    img

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-
+import Head from "next/head";
 export const metadata = {
   title: "Modi Jass | Monetize on X",
   description:
@@ -9,6 +9,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Modi Jass | Monetize on X</title>
+        <meta
+          name="description"
+          content="Monetize on X I help you build and scale your brand create better content and sign more clients with my 3-step system in 30 days"
+          key="desc"
+        />
+        <meta property="og:title" content="Modi Jass | Monetize on X" />
+        <meta
+          property="og:description"
+          content="Monetize on X I help you build and scale your brand create better content and sign more clients with my 3-step system in 30 days"
+        />
+        <meta property="og:image" content="/src/assets/hero/modipfp.webp" />
+      </Head>
       <body>{children}</body>
     </html>
   );
