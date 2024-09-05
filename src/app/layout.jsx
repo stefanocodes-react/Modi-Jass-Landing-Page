dd98d2b3e79b874250ca5e3531a632646e95d413
modi <modijass86@gmail.com>
2024-09-05 16:07:45 +0300

    updated ogimg

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
         />
         <meta property="og:url" content="https://www.moayadj.com/" />
         <meta property="og:site_name" content="Modi Jass" />
-        <meta property="og:image" content="/src/assets/modipfp.webp" />
+        <meta property="og:image" content="modipfp.webp" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Modi Jass" />
         <meta
